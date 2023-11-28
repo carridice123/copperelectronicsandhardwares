251835c0207344faa7c8605a026d1c78dc78ae4d
Raheem Carridice <carridiceraheem871@gmail.com>
2023-11-28 12:56:46 -0500

    fix desktop nav also style current page for index and electronics

---
+++ js/desktoptester.js
@@ -0,0 +1,21 @@
+const pathName = window.location.pathname;
+const pageName = pathName.split("/").pop();
+
+if(pageName === "index.html"){
+    document.querySelector(".home").classList.add('active')
+}
+
+if(pageName === "electronics.html"){
+    document.querySelector(".electronicspage").classList.add('active')
+}
+
+if(pageName === "constructions.html"){
+    document.querySelector(".constructionpage").classList.add('active')
+}
+if(pageName === "events.html"){
+    document.querySelector(".eventspage").classList.add('active')
+}
+if(pageName === "rentals.html"){
+    document.querySelector(".rentalcarspage").classList.add('active')
+}
+
